b0382badbc1484d66488967b6c73e705a011db70
Fabio Mendes <fabiojmendes@gmail.com>
2024-08-14 02:40:49 +0000

    Improving error handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::{
+    collections::HashMap,
+    time::{SystemTime, UNIX_EPOCH},
+};
 
 use bluer::{AdapterEvent, Session};
 use byteorder::{ByteOrder, LittleEndian};
@@ -32,8 +35,17 @@ struct Config {
 struct TempReading {
     version: u8,
     counter: u8,
-    temp: f32,
     voltage: u16,
+    temp: i16,
+}
+
+impl TempReading {
+    fn temperature(&self) -> Result<f32, String> {
+        if self.temp == i16::MAX {
+            return Err(format!("Invalid temperature {}", self.temp));
+        }
+        Ok(f32::from(self.temp) / 100.0)
+    }
 }
 
 impl TryFrom<&[u8]> for TempReading {
@@ -45,18 +57,14 @@ impl TryFrom<&[u8]> for TempReading {
         }
         let version = data[0];
         let counter = data[1];
-        let raw_temp = LittleEndian::read_i16(&data[2..4]);
-        if raw_temp == i16::MIN {
-            return Err("Invalid temperature");
-        }
-        let temp = f32::from(raw_temp) / 100.0;
-        let voltage = LittleEndian::read_u16(&data[4..]);
+        let voltage = LittleEndian::read_u16(&data[2..4]);
+        let temp = LittleEndian::read_i16(&data[4..]);
 
         Ok(TempReading {
             version,
             counter,
-            temp,
             voltage,
+            temp,
         })
     }
 }
@@ -70,8 +78,8 @@ fn timestamp_nanos() -> u128 {
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> bluer::Result<()> {
-    let config = Config::parse();
     env_logger::builder().format_timestamp(None).init();
+    let config = Config::parse();
 
     let mut opts = MqttOptions::new(config.id, config.host, config.port);
     opts.set_credentials(config.username, config.password);
@@ -87,36 +95,54 @@ async fn main() -> bluer::Result<()> {
         );
         adapter.set_powered(true).await?;
 
-        let mut last_count = 0u8;
+        let mut last_counts = HashMap::new();
 
         let mut device_events = adapter.discover_devices_with_changes().await?;
         while let Some(evt) = device_events.next().await {
             if let AdapterEvent::DeviceAdded(addr) = evt {
                 let device = adapter.device(addr)?;
+                log::trace!("Device: {:?} name: {:?}", device, device.name().await?);
                 if device.name().await? != Some(String::from("Tempsys")) {
                     continue;
                 }
                 let rssi = device.rssi().await?.unwrap_or(0);
-                if let Some(reading) = device
+                match device
                     .manufacturer_data()
                     .await?
                     .and_then(|mut d| d.remove(&0xffff))
-                    .and_then(|data| TempReading::try_from(&*data).ok())
+                    .map(|data| TempReading::try_from(&*data))
                 {
-                    if reading.counter == last_count {
-                        continue;
+                    Some(Ok(reading)) => {
+                        if Some(&reading.counter) == last_counts.get(&addr) {
+                            continue;
+                        }
+                        last_counts.insert(addr, reading.counter);
+                        let timestamp = timestamp_nanos();
+                        let payload = match reading.temperature() {
+                            Ok(temp) => {
+                                format!("sensor,sender={},version={} temperature={:.2},voltage={},rssi={} {}",
+                                            addr, reading.version, temp, reading.voltage, rssi, timestamp)
+                            }
+                            Err(e) => {
+                                log::warn!("Error parsing temperature: {}", e);
+                                format!(
+                                    "sensor,sender={},version={} voltage={},rssi={} {}",
+                                    addr, reading.version, reading.voltage, rssi, timestamp
+                                )
+                            }
+                        };
+                        log::info!("{} (counter={})", payload, reading.counter);
+                        client
+                            .publish(&config.topic, QoS::AtLeastOnce, false, payload)
+                            .await
+                            .unwrap();
                     }
-                    last_count = reading.counter;
-                    let timestamp = timestamp_nanos();
-                    let payload = format!(
-                        "sensor,sender={},version={} temperature={:.2},voltage={},rssi={} {}",
-                        addr, reading.version, reading.temp, reading.voltage, rssi, timestamp
-                    );
-                    log::info!("{} counter={}", payload, reading.counter);
-                    client
-                        .publish(&config.topic, QoS::AtLeastOnce, false, payload)
-                        .await
-                        .unwrap();
+                    Some(Err(e)) => {
+                        log::error!("Error reading manufacturer data {}", e);
+                    }
+                    None => {
+                        log::warn!("Manufacurer data not found");
+                    }
                 }
             }
         }
